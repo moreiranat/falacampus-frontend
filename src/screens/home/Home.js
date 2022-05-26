116c3295f69aa4d72fad5aecc3464cddb1a747e0
Nataly Lucena <natalylucena.pb@gmail.com>
2022-05-25 22:24:04 -0300

    Update screen Home

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -13,7 +13,7 @@ class Home extends React.Component {
                     <div className='col-md-12' style={this.styles.colMd12}>
                         <div className="bs-docs-section">
 
-                            <div className="card bg-light mb-3" style={this.styles.cardBg}>
+                            <div className="card border-success mb-3" style={this.styles.cardBg}>
                                 <h1 className="card-header text-center">Bem Vindo(a) ao "Fala Campus"</h1>
                                 <h3 className="card-header text-center">Este espaço é destinado a toda a comunidade acadêmica, onde todos poderão propor sugestões, realizar críticas e elogios, relacionados ao Instituto Federal da Paraíba, Campus Monteiro - PB.</h3>
                                 <h4 className="card-header text-center">Projeto desenvolvido por Nataly Lucena, Patrícia dos Santos e Rosenato Barreto para a Disciplina de Desenvolvimento de Aplicações Corporativas com o Professor Elenilson Vieira, no 5° Período do Curso de Análise e Desenvolvimento de Sistemas do IFPB, Campus Monteiro/PB</h4>
